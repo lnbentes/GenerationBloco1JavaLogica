9b64a084da6e377a02ef79781432bef91d4da497
lucas bentes <lucasbentes16@gmail.com>
2022-03-04 11:19:35 -0300

    condicional 3.0

--- src/br/com/generation48/lucasbentes/condicional/Exercicio2.java
+++ src/br/com/generation48/lucasbentes/condicional/Exercicio2.java
@@ -1,7 +1,5 @@
 package br.com.generation48.lucasbentes.condicional;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Exercicio2 {
@@ -9,19 +7,42 @@ public class Exercicio2 {
 	public static void main(String[] args) {
 		/*Faça um programa que entre com três números e coloque em ordem crescente.*/
 		
-		ArrayList<Double> lista = new ArrayList<>();
 		Scanner ler = new Scanner(System.in);
-		
+		int a, b, c;
+		int lista[] = new int[3];
 		System.out.println("Entre com 3 numeros inteiros: ");
-		lista.add(ler.nextDouble());
-		lista.add(ler.nextDouble());
-		lista.add(ler.nextDouble());
+		a = ler.nextInt();
+		b = ler.nextInt();
+		c = ler.nextInt();
 		
-		Collections.sort(lista);
+		if(a > b && a > c) {
+			lista[0] = a;
+		}else if(b > a && b > c) {
+			lista[0] = b;
+		}else if(c > a && c > b) {
+			lista[0] = c;
+		}
 		
-		System.out.println("Os numeros em ordem crescente: " + lista);
+		if(a < b && a > c || a > b && a < c) {
+			lista[1] = a;
+		}else if(b < a && b > c || b > a && b < c) {
+			lista[1] = b;
+		}else if(c < a && c > b || c > a && c < b) {
+			lista[1] = c;
+		}
+		
+		if(a < b && a < c) {
+			lista[2] = a;
+		}else if(b < a && b < c) {
+			lista[2] = b;
+		}else if(c < a && c < b) {
+			lista[2] = c;
+		}
+		
+		System.out.println("Os numeros em ordem crescente: " + lista[0] + " " + lista[1] + " " + lista[2]);
 		
 		ler.close();
+		
 
 	}
 
---
+++ src/br/com/generation48/lucasbentes/laco/Exercicio1.java
@@ -0,0 +1,20 @@
+package br.com.generation48.lucasbentes.laco;
+
+import java.util.ArrayList;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		/*Informar todos os números de 1000 a 1999 que quando divididos por 11
+obtemos resto = 5. (FOR)*/
+		
+		double numero;
+		ArrayList<Double> lista = new ArrayList<>();
+		
+		for(int i = 1000; i <= 1999; i++) {
+			
+		}
+
+	}
+
+}
